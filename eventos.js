8aad38f13d23f0ee18b71168325accb775f408cb
NicolasSequeira11 <nicolassequeira1997@hotmail.com>
2023-08-08 15:25:51 -0300

    Solución Click

--- eventos.js
+++ eventos.js
@@ -1,5 +1,11 @@
 const container = document.querySelector("#container");
+const button = document.querySelector("#button");
+
+button.addEventListener("click", function(){
+    alert("Hola!");
+});
 
 container.addEventListener("click", function(){
     alert("Hola! Soy el div");
 });
+
